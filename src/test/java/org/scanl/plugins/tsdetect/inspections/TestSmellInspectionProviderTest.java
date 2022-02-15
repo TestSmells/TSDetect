a37d013b21dcae94a75d66ae580df9e93891f06c
nxb9626 <nxb9626@rit.edu>
2022-02-15 11:33:14 -0500

    [#31] Adjusting to alphabetical order

--- src/test/java/org/scanl/plugins/tsdetect/inspections/TestSmellInspectionProviderTest.java
+++ src/test/java/org/scanl/plugins/tsdetect/inspections/TestSmellInspectionProviderTest.java
@@ -17,8 +17,8 @@ public class TestSmellInspectionProviderTest extends LightJavaCodeInsightFixture
 
 	public void testGetInspectionClasses(){
 		Class[] expectedClasses = new Class[]{
-				EmptyMethodInspection.class,
 				DuplicateAssertInspection.class,
+				EmptyMethodInspection.class,
 				GeneralFixtureInspection.class,
 				LazyTestInspection.class
 		};
