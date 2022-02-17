e8ab967b1dee413147783ac2dd6546967c7e2f97
ses7578 <ses7578@rit.edu>
2022-02-17 17:36:35 -0500

    Default Test Done

--- src/main/java/org/scanl/plugins/tsdetect/model/SmellType.java
+++ src/main/java/org/scanl/plugins/tsdetect/model/SmellType.java
@@ -5,9 +5,10 @@ package org.scanl.plugins.tsdetect.model;
  * everything with the action should work
  */
 public enum SmellType {
+    DEFAULT_TEST,
+    DUPLICATE_ASSERT,
     EMPTY_TEST,
     EAGER_TEST,
-    DUPLICATE_ASSERT,
     GENERAL_FIXTURE,
     LAZY_TEST,
     REDUNDANT_PRINT
