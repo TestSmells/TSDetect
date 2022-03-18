30d2dbfd1bee5d54538464a0e8a2e45c75fb3890
nxb9626 <nxb9626@rit.edu>
2022-03-18 17:30:56 -0400

    [#27] Code complete, still need to verify

--- src/main/java/org/scanl/plugins/tsdetect/inspections/AssertionRouletteInspection.java
+++ src/main/java/org/scanl/plugins/tsdetect/inspections/AssertionRouletteInspection.java
@@ -16,6 +16,18 @@ import java.util.*;
  */
 public class AssertionRouletteInspection extends SmellInspection {
 	HashMap<String, List<PsiElement>> asserts = new HashMap<>();
+	HashSet<String> oneParams = new HashSet<>(
+			Arrays.asList("assertFalse",
+					"assertNotNull",
+					"assertNull",
+					"assertTrue"
+			));
+//	HashSet<String> twoParams = new HashSet<>(
+//			Arrays.asList("assertFalse",
+//					"assertNotNull",
+//					"assertNull",
+//					"assertTrue"
+//			));
 
 	@Override
 	public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
@@ -47,26 +59,40 @@ public class AssertionRouletteInspection extends SmellInspection {
 	public boolean hasSmell(PsiElement element) {
 		if (!PluginSettings.GetSetting(getSmellType().toString())) return false;
 		boolean output = false;
-		if(element instanceof PsiMethod) {
+
+		if(shouldTestElement(element)) {
 			List<PsiMethodCallExpression> x = getMethodExpressions((PsiMethod) element);
 			for (PsiMethodCallExpression psiMethodCallExpression : x) {
 				int count = psiMethodCallExpression.getArgumentList().getExpressionCount();
 				String key = psiMethodCallExpression.getMethodExpression().getQualifiedName();
-				if(count < 3){
-					if(asserts.containsKey(key)){
-						asserts.get(key).add(psiMethodCallExpression);
-						output = true;
+
+				if (key.equals("fail")) {
+					output = found(key,psiMethodCallExpression);
+				} else if (key.startsWith("assert")) {
+					if(oneParams.contains(key)){
+						if (count < 2){
+							output = found(key,psiMethodCallExpression);
+						}
 					}
-					else{
-						asserts.put(key, new ArrayList<>());
-						asserts.get(key).add(psiMethodCallExpression);
+					else if (count < 3) {
+						output = found(key,psiMethodCallExpression);
 					}
 				}
 			}
 		}
 		return output;
 	}
+private boolean found(String key, PsiMethodCallExpression psiMethodCallExpression){
 
+	if (asserts.containsKey(key)) {
+		asserts.get(key).add(psiMethodCallExpression);
+		return true;
+	} else {
+		asserts.put(key, new ArrayList<>());
+		asserts.get(key).add(psiMethodCallExpression);
+		return false;
+	}
+}
 	/**
 	 * Gets the matching smell type enum
 	 * @return The Smell Type Enum
