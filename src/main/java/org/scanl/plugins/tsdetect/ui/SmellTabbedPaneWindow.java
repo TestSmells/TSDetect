529cefbbeb00a7d3edea1f561c3132d71bfc2b17
Anthony Peruma <shehan.peruma@gmail.com>
2022-10-04 10:53:54 -1000

    Export report (#125)
    * added dialog to export reports
    
    * updated
    
    * added dialog to export reports
    
    * updated
    
    * code cleanup

--- src/main/java/org/scanl/plugins/tsdetect/ui/SmellTabbedPaneWindow.java
+++ src/main/java/org/scanl/plugins/tsdetect/ui/SmellTabbedPaneWindow.java
@@ -1,55 +1,41 @@
 package org.scanl.plugins.tsdetect.ui;
 
-import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiJavaFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.search.FileTypeIndex;
-import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.uiDesigner.core.GridConstraints;
-import org.scanl.plugins.tsdetect.SmellVisitor;
 import org.scanl.plugins.tsdetect.common.PluginResourceBundle;
 import org.scanl.plugins.tsdetect.common.Util;
 import org.scanl.plugins.tsdetect.model.ExecutionResult;
-import org.scanl.plugins.tsdetect.model.InspectionClassModel;
-import org.scanl.plugins.tsdetect.model.InspectionMethodModel;
 import org.scanl.plugins.tsdetect.service.Analyzer;
+import org.scanl.plugins.tsdetect.ui.dialogs.ExportReport;
 import org.scanl.plugins.tsdetect.ui.tabs.TabDetectedSmellTypes;
-import org.scanl.plugins.tsdetect.ui.tabs.TabSmellDistribution;
 import org.scanl.plugins.tsdetect.ui.tabs.TabInfectedFiles;
+import org.scanl.plugins.tsdetect.ui.tabs.TabSmellDistribution;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import static com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH;
 
 public class SmellTabbedPaneWindow {
-//    private final List<InspectionMethodModel> allMethods = new ArrayList<>();
-//    private final List<InspectionClassModel> allClasses = new ArrayList<>();
-    private ExecutionResult executionResult;
 
+    private final JPanel panelSmellDistribution;
+    private final JPanel panelDetectedSmellTypes;
+    private final JPanel panelInfectedFiles;
+    private final TabDetectedSmellTypes tabDetectedSmellTypes;
+    private final TabSmellDistribution tabSmells;
+    private final TabInfectedFiles tabInfectedFiles;
+    private ExecutionResult executionResult;
     private JPanel panelInspection;
     private JButton buttonAnalyzeProject;
     private JLabel labelLoading;
     private JTabbedPane tabbedPane;
     private JLabel labelExecution;
-
-    private final JPanel panelSmellDistribution;
-    private final JPanel panelDetectedSmellTypes;
-    private final JPanel panelInfectedFiles;
-
-    private TabDetectedSmellTypes tabDetectedSmellTypes;
-    private TabSmellDistribution tabSmells;
-    private TabInfectedFiles tabInfectedFiles;
+    private JButton buttonExport;
 
     public SmellTabbedPaneWindow() {
         buttonAnalyzeProject.setName(PluginResourceBundle.message(PluginResourceBundle.Type.UI, "BUTTON.ANALYSIS.NAME"));
@@ -81,14 +67,17 @@ public class SmellTabbedPaneWindow {
         labelLoading.setIcon(Util.GetLoadingIcon());
         labelLoading.setVisible(false);
         labelExecution.setVisible(false);
+        buttonExport.setVisible(false);
 
         tabDetectedSmellTypes = new TabDetectedSmellTypes();
         tabInfectedFiles = new TabInfectedFiles();
         tabSmells = new TabSmellDistribution();
+        buttonExport.setVisible(false);
 
         buttonAnalyzeProject.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                buttonExport.setVisible(false);
                 labelExecution.setText("");
                 labelExecution.setVisible(false);
                 labelLoading.setText("");
@@ -113,22 +102,22 @@ public class SmellTabbedPaneWindow {
                                         GridConstraints gc = new GridConstraints();
                                         gc.setFill(FILL_BOTH);
 
-                                        tabDetectedSmellTypes.LoadSmellyData(executionResult.getAllClasses(),executionResult.getAllMethods());
-                                        JPanel content1  = tabDetectedSmellTypes.GetContent();
+                                        tabDetectedSmellTypes.LoadSmellyData(executionResult.getAllClasses(), executionResult.getAllMethods());
+                                        JPanel content1 = tabDetectedSmellTypes.GetContent();
                                         content1.setVisible(true);
                                         panelDetectedSmellTypes.setLayout(new java.awt.BorderLayout());
                                         panelDetectedSmellTypes.add(content1);
                                         panelDetectedSmellTypes.validate();
 
-                                        tabInfectedFiles.LoadSmellyData(executionResult.getAllClasses(),executionResult.getAllMethods());
-                                        JPanel content2  = tabInfectedFiles.GetContent();
+                                        tabInfectedFiles.LoadSmellyData(executionResult.getAllClasses(), executionResult.getAllMethods());
+                                        JPanel content2 = tabInfectedFiles.GetContent();
                                         content2.setVisible(true);
                                         panelInfectedFiles.setLayout(new java.awt.BorderLayout());
                                         panelInfectedFiles.add(content2);
                                         panelInfectedFiles.validate();
 
-                                        tabSmells.LoadSmellyData(executionResult.getAllClasses(),executionResult.getAllMethods());
-                                        JPanel content3  = tabSmells.GetContent();
+                                        tabSmells.LoadSmellyData(executionResult.getAllClasses(), executionResult.getAllMethods());
+                                        JPanel content3 = tabSmells.GetContent();
                                         content3.setVisible(true);
                                         panelSmellDistribution.setLayout(new java.awt.BorderLayout());
                                         panelSmellDistribution.add(content3);
@@ -137,9 +126,10 @@ public class SmellTabbedPaneWindow {
                                         panelInspection.add(tabbedPane, gc);
                                         labelLoading.setVisible(false);
                                         String pattern = PluginResourceBundle.message(PluginResourceBundle.Type.UI, ("LABEL.EXECUTION.TEXT"));
-                                        String message = MessageFormat.format(pattern, executionResult.getExecutionTimestampString() ,executionResult.getExecutionDurationInSeconds());
+                                        String message = MessageFormat.format(pattern, executionResult.getExecutionTimestampString(), executionResult.getExecutionDurationInSeconds());
                                         labelExecution.setText(message);
                                         labelExecution.setVisible(true);
+                                        buttonExport.setVisible(true);
                                         panelInspection.updateUI();
                                         panelInspection.updateUI();
                                     }
@@ -148,6 +138,17 @@ public class SmellTabbedPaneWindow {
 
                         })).start();
 
+            }
+        });
+
+        buttonExport.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Window parentWindow = SwingUtilities.windowForComponent(panelInspection);
+                Frame parentFrame = (Frame) parentWindow;
+
+                ExportReport exportReportDialog = new ExportReport(parentFrame, executionResult);
+                exportReportDialog.setVisible(true);
             }
         });
     }
