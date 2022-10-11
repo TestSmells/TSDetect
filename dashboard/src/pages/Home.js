7a9d3a4de74a443cf0081493c7a61fc02278c5b3
Spencer Stissi <sas7359@g.rit.edu>
2022-10-11 19:34:28 -0400

    populate landing page

---
+++ dashboard/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+
+function Home() {
+    return(
+        <>
+            <Row>
+                <h1 className="page-title" >Landing Page</h1>
+                <p>
+                    Words
+                </p>
+            </Row>
+        </>
+    )
+}
+
+export default Home;
