6d40496d6509de049596118fb81ddf1da9ed3efe
Ben Radley <bdr1224@rit.edu>
2022-10-17 19:04:14 -0400

    Added popupfactory methods

--- src/main/java/org/scanl/plugins/tsdetect/ui/popup/Popup.java
+++ src/main/java/org/scanl/plugins/tsdetect/ui/popup/Popup.java
@@ -6,7 +6,10 @@ import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 
 public class Popup{
-    private final JOptionPane consentForm;
+    javax.swing.Popup pop;
+    PopupFactory pf;
+    private final JFrame frame;
+    private final JPanel consentForm;
     private final JButton confirm;
     private final JButton deny;
     //Initialized to null so it runs the first time
@@ -14,7 +17,10 @@ public class Popup{
     private final JLabel infor;
 
     public Popup() {
-        consentForm = new JOptionPane();
+        consentForm = new JPanel();
+        frame = new JFrame("Consent Form");
+        frame.setSize(400,400);
+        pf = new PopupFactory();
         infor = new JLabel("Do you consent to sending anonymous test smell data for research purposes?");
         confirm = new JButton();
         deny = new JButton();
@@ -22,7 +28,9 @@ public class Popup{
             consentForm.add(infor);
             consentForm.add(confirm);
             consentForm.add(deny);
-            consentForm.setVisible(true);
+            frame.add(consentForm);
+            pop = pf.getPopup(frame, consentForm, 180, 100);
+            frame.setVisible(true);
             confirm.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
                     choice = true;
@@ -33,6 +41,10 @@ public class Popup{
                     choice = false;
                 }
             });
+
         }
+    }
+    public static void main(String args[]){
+        Popup p = new Popup();
     }
 }
