752826ce4033c552bf6b8872c6021b4c14ad2b05
Spencer Stissi <sas7359@g.rit.edu>
2022-11-06 15:14:09 -0500

    setup for docker config

---
+++ server/src/main/java/org/testsmells/server/configuration/DockerDatasourceConfig.java
@@ -0,0 +1,21 @@
+package org.testsmells.server.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DockerDatasourceConfig {
+
+    /*
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public HikariDataSource getDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+    */
+
+
+}
