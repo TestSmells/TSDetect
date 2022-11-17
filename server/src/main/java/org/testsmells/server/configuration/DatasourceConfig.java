6e46377b5b91ca77833cb3f3a0840daf69fc923c
Spencer Stissi <sas7359@g.rit.edu>
2022-11-16 19:30:09 -0500

    add multiple datasources

--- server/src/main/java/org/testsmells/server/configuration/DatasourceConfig.java
+++ server/src/main/java/org/testsmells/server/configuration/DatasourceConfig.java
@@ -1,17 +1,28 @@
 package org.testsmells.server.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatasourceConfig {
 
     @Bean
-    @ConfigurationProperties("spring.datasource")
-    public HikariDataSource getDataSource() {
+    @Qualifier("ds-plugin")
+    @Primary
+    @ConfigurationProperties("spring.datasource.plugin")
+    public HikariDataSource getPluginDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @Qualifier("ds-dashboard")
+    @ConfigurationProperties("spring.datasource.dashboard")
+    public HikariDataSource getDashboardDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 
--- server/src/main/java/org/testsmells/server/repository/ExampleRepository.java
+++ server/src/main/java/org/testsmells/server/repository/ExampleRepository.java
@@ -4,6 +4,7 @@ import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.ResultQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import org.testsmells.server.tables.pojos.TestSmells;
 
@@ -13,12 +14,15 @@ import java.util.List;
 @Repository
 public class ExampleRepository {
 
-    @Autowired
-    DSLContext dsl;
+    private final DSLContext dashboardDsl;
+
+    public ExampleRepository(@Qualifier("dsl-dashboard") DSLContext dashboardDsl) {
+        this.dashboardDsl = dashboardDsl;
+    }
 
     public List<TestSmells> getSmellTypes() throws SQLException {
         final String queryString = "SELECT * FROM tsdetect.test_smells";
-        try (ResultQuery<Record> query = dsl.resultQuery(queryString)) {
+        try (ResultQuery<Record> query = dashboardDsl.resultQuery(queryString)) {
             return query.fetchInto(TestSmells.class);
         }
     }
