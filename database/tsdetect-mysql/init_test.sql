a32c16af5582f7ab8501df3dbbf4cb8e93bfd152
jks6634 <54516972+jks6634@users.noreply.github.com>
2022-11-16 20:07:18 -0500

    Adding remote users for non-local connections, and giving plugin read privilages so it can dynamically find smell and run numbers

--- database/tsdetect-mysql/init_test.sql
+++ database/tsdetect-mysql/init_test.sql
@@ -33,9 +33,18 @@ CREATE TABLE `tsdetect`.`test_run_smells` (
 CREATE USER 'plugin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT INSERT ON tsdetect.test_runs TO 'plugin'@'localhost';
 GRANT INSERT ON tsdetect.test_run_smells TO 'plugin'@'localhost';
+GRANT SELECT ON tsdetect.* TO 'dashboard'@'localhost';
+
+CREATE USER 'plugin'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT INSERT ON tsdetect.test_runs TO 'plugin'@'%';
+GRANT INSERT ON tsdetect.test_run_smells TO 'plugin'@'%';
+GRANT SELECT ON tsdetect.* TO 'plugin'@'%';
 
 CREATE USER 'dashboard'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT ON tsdetect.* TO 'dashboard'@'localhost';
+
+CREATE USER 'dashboard'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT ON tsdetect.* TO 'dashboard'@'%';
 
 -- Update test smells table with data
 USE tsdetect;
