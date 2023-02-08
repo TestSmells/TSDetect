4314845820f6c9cfc3226787259f338d82011941
bdr1224 <bdr1224@rit.edu>
2023-02-08 15:10:36 -0500

    Refactored class to match new API specification and retrieve timestamp and uuid from non-static hashmap locations

--- server/src/main/java/org/testsmells/server/controller/requests/TestResults.java
+++ server/src/main/java/org/testsmells/server/controller/requests/TestResults.java
@@ -10,84 +10,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestResults {
-    @NotNull
-    private String userID;
-    @NotNull
-    private String timestamp;
-    @NotNull
-    private Map<String, Integer> smellCount;
 
-
-    private ArrayList<String> vars;
-
-    public TestResults(String userID, String timestamp, Map<String, Integer> smellCount, ArrayList<String> vars) {
-        this.userID = userID;
-        this.timestamp = timestamp;
-        this.smellCount = smellCount;
-        this.vars = vars;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Map<String, Integer> getSmellCount() {
-        return smellCount;
-    }
-
-    public void setSmellCount(Map<String, Integer> smellCount) {
-        this.smellCount = smellCount;
-    }
-
-    public ArrayList<String> getVars() {
-        return vars;
-    }
-
-    public void setVars(ArrayList<String> vars) {
-        this.vars = vars;
-    }
-
+    /*
+        Method for taking the incoming JSON strong from the plugin, deserializing it, verifying the timestamp and uuid
+        are correct, then passing the body to the database API, returning a 200 response in the event of success
+        Any other HTTP should be considered an error
+     */
     @PostMapping(value = "/test-results", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Object> testResults(@RequestBody String json) throws Exception{
         ObjectMapper mapper = new ObjectMapper();
-        TypeReference<HashMap<String, Integer>> typeRef = new TypeReference<HashMap<String, Integer>>() {};
-        Map<String, Integer> map = mapper.readValue(json, typeRef);
-        for(String key: map.keySet()){
-            vars.add(key);
-        }
-        String uid = vars.get(0);
-        String ts = vars.get(1);
+        TypeReference<HashMap<String, String>> typeRef = new TypeReference<>() {};
+        Map<String, String> map = mapper.readValue(json, typeRef);
 
-        if(isValidUid(uid) && isValidDate(ts)){
+        String uuid = map.get("uuid");
+        String ts = map.get("timestamp");
+
+        //Verifies the uuid and timestamp match the enforced format
+        if(isValidUuid(uuid) && isValidDate(ts)){
 
         }
 
         return ResponseEntity.ok().build();
     }
-    public boolean isValidUid(String uid){
+    public boolean isValidUuid(String uuid){
         //TODO: fill this out when UID generation is implemented
         return true;
     }
     public boolean isValidDate(String date){
-        SimpleDateFormat sd = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        //example correct timestamp is "2023-02-07 18:26:50.952"
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
         sd.setLenient(false);
         try {
             sd.parse(date.trim());
