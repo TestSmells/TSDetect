5efc30a4d9f3ed6c53637bbd57d2f39bd2a70a48
cmr6689 <cmr6689@g.rit.edu>
2023-02-27 21:04:45 -0500

    chart font size increased

--- dashboard/src/components/SmellGraph.js
+++ dashboard/src/components/SmellGraph.js
@@ -2,6 +2,41 @@ import React from "react"
 import { Bar } from "react-chartjs-2";
 
 export default function SmellGraph({ data }) {
+    const options = {
+        scales: {
+            x: {
+                ticks: {
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            y: {
+                ticks: {
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            tooltip: {
+                titleFont: {
+                    size: 16
+                },
+                bodyFont: {
+                    size: 16
+                }
+            }
+        }
+    }
     return(
         <Bar
             className="padding-left-right"
@@ -16,6 +51,7 @@ export default function SmellGraph({ data }) {
                     }]
                 }
             }
+            options={options}
         />
     )
 }
