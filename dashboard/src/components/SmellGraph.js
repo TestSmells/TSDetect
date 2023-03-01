596560954167b34c7923d7eebe8b16efddb4a950
cmr6689 <cmr6689@g.rit.edu>
2023-02-28 19:44:29 -0500

    added font size picker and color/font persistence on reload

--- dashboard/src/components/SmellGraph.js
+++ dashboard/src/components/SmellGraph.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react"
 import { Bar } from "react-chartjs-2";
 import Sketch from "@uiw/react-color-sketch";
-import {Dropdown} from "react-bootstrap";
+import {Col, Dropdown, DropdownButton, Row} from "react-bootstrap";
 
 export default function SmellGraph({ data }) {
-    const [hex, setHex] = useState('#009688')
+    const [hex, setHex] = useState(localStorage.getItem('hex') !== null ? localStorage.getItem('hex') : '#009688')
+    const [fontSize, setFontSize] = useState(localStorage.getItem('fontSize') != null ? localStorage.getItem('fontSize') : 16)
 
-    const fontSize = 16
     const options = {
         scales: {
             x: {
@@ -44,21 +44,44 @@ export default function SmellGraph({ data }) {
     }
     return(
         <>
-            <Dropdown autoClose="outside">
-                <Dropdown.Toggle variant='secondary'>
-                    Color
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item>
-                        <Sketch
-                            color={hex}
-                            onChange={(color) => {
-                                setHex(color.hex)
-                            }}
-                        />
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+            <Row>
+                <Col>
+                    <Dropdown autoClose="outside">
+                        <Dropdown.Toggle variant='secondary'>
+                            Color
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>
+                                <Sketch
+                                    color={hex}
+                                    onChange={(color) => {
+                                        setHex(color.hex)
+                                        localStorage.setItem('hex', color.hex)
+                                    }}
+                                />
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+                <Col>
+                    <DropdownButton
+                        title="Font"
+                        variant="secondary"
+                        id="fontDropdown"
+                        onSelect={(size) => {
+                            setFontSize(size)
+                            localStorage.setItem('fontSize', size)
+                        }}
+                    >
+                        <Dropdown.Item eventKey="12">12</Dropdown.Item>
+                        <Dropdown.Item eventKey="16">16</Dropdown.Item>
+                        <Dropdown.Item eventKey="20">20</Dropdown.Item>
+                        <Dropdown.Item eventKey="24">24</Dropdown.Item>
+                        <Dropdown.Item eventKey="28">28</Dropdown.Item>
+                        <Dropdown.Item eventKey="32">32</Dropdown.Item>
+                    </DropdownButton>
+                </Col>
+            </Row>
             <Bar
                 className="padding-left-right"
                 data={
