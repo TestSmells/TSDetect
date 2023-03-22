077eeb28ec669d43d263a148aa9b88cee88c778b
Spencer Stissi <sas7359@g.rit.edu>
2023-03-22 18:12:43 -0400

    remove jooq class generation

--- server/src/main/java/org/testsmells/server/service/DashboardService.java
+++ server/src/main/java/org/testsmells/server/service/DashboardService.java
@@ -3,9 +3,6 @@ package org.testsmells.server.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.testsmells.server.repository.DBOutputTool;
-import org.testsmells.server.tables.pojos.TestSmells;
-
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.*;
 
